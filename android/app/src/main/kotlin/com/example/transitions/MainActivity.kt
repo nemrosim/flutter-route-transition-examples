67b1135078235cecf43e83940289270c54d66c39
nemrosim <nemrosim1988@gmail.com>
2022-04-20 23:26:27 +0300

    start

---
+++ android/app/src/main/kotlin/com/example/transitions/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.transitions
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => const HomePage(),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/second': (context) => const Page2(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home page'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            ElevatedButton(
+              child: const Text('Simple transition'),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/second');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Page 2'),
+        ),
+        body: const Center(
+            child: Text(
+          'Page 2',
+          style: TextStyle(fontSize: 30),
+        )));
+  }
+}
